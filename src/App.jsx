046b87bc844aeba22cb44137bbca784c6e2890eb
emmy grammy <ojeabuo24@gmail.com>
2026-05-06 00:49:45 +0100

    src

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ function App() {
   
   return (
     <>
-      <h1 className="text-3xl font-bold text-blue-500">
+      <h1 className="text-3xl font-bold text-sans text-blue-500">
   Tailwind is working 🚀
       </h1>
+
+       <h2 className="text-2xl font-bold text-sans text-blue-500">
+  Tailwind is working 🚀
+      </h2>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,54 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF91A4",
+        primaryHover: "#FF7A90",
+
+        secondary: "#0F52BA",
+        secondaryHover: "#0C45A0",
+
+        background: "#F4F4F4",
+        surface: "#FFFFFF",
+
+        textPrimary: "#000000",
+        textSecondary: "#6B6B6B",
+
+        border: "#E0E0E0"
+      },
+
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+
+      fontSize: {
+        h1: ["32px", "40px"],
+        h2: ["24px", "32px"],
+        h3: ["20px", "28px"],
+        body: ["16px", "24px"],
+        small: ["14px", "20px"],
+        caption: ["12px", "16px"],
+      },
+
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+
+      spacing: {
+        section: "64px",
+        card: "24px",
+      },
+
+      borderRadius: {
+        sm: "6px",
+        md: "12px",
+        lg: "16px",
+      },
+    },
   },
   plugins: [],
 }
