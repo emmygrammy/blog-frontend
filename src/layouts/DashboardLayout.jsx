e900f363231f7a77eb2b89e15c882bf654cec3de
emmy grammy <ojeabuo24@gmail.com>
2026-05-24 05:46:20 +0100

    Blog-frontend

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,27 +1,49 @@
 import { Outlet, Link } from 'react-router-dom'
+import Logo from '../assets/Logo.svg'
+import { FaHome, FaPlus, FaList } from 'react-icons/fa'
+
+
 
 function DashboardLayout() {
   return (
-    <div className="min-h-screen flex bg-gray-100">
+    <div className="min-h-screen flex bg-white">
+
+
+   
+
 
       {/* Sidebar */}
-      <aside className="w-64 bg-primary text-white p-6">
+      <aside className="w-64 bg-gray-100 text-white p-4 block">
+          
+           {/* logo */}
 
-        <h1 className="text-3xl mb-10 font-heading">
+         <img src={Logo} 
+         alt="logo" 
+         className="w-36 h-36 -mt-16" 
+         />
+
+        {/* <h1 
+        className="text-3xl mb-10 font-heading">
           Admin Panel
-        </h1>
+        </h1> */}
 
         <nav className="flex flex-col gap-4">
 
-          <Link to="/dashboard">
+          <Link  className="text-primary font-heading text-lg flex items-center gap-2" 
+          to="/dashboard">
+           <FaHome className="text-xl" />
             Dashboard
           </Link>
 
-          <Link to="/dashboard/create">
+          <Link  className="text-primary font-heading text-lg flex items-center gap-2" 
+          to="/dashboard/create">
+             <FaPlus className="text-xl" />
             Create Blog
           </Link>
 
-          <Link to="/dashboard/manage">
+          <Link  className="text-primary font-heading text-lg flex items-center gap-2" 
+          to="/dashboard/manage">
+             <FaList className="text-xl" />
             Manage Blogs
           </Link>
 
