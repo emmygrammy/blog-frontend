0d22a0ebd9374afa75304aaeb32c7977b2817b2e
emmy grammy <ojeabuo24@gmail.com>
2026-06-19 23:39:26 +0100

    Blog-frontend

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -17,7 +17,7 @@ function NotFound() {
         </h2>
 
         {/* Message */}
-        <p className="text-gray-600 mt-3 mb-6 font-heading">
+        <p className="text-gray-600 mt-3 mb-6 font-heading text-center">
           Sorry, the page you are looking for doesn’t exist or has been moved.
         </p>
 
